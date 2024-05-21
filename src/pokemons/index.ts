bd60482b164c93080514eaef250ed738a3dc50e8
Guzmán Fernando Nahuel <nahuel1432@gmail.com>
2024-05-21 17:54:39 -0300

    .

--- src/pokemons/index.ts
+++ src/pokemons/index.ts
@@ -1,3 +1,5 @@
 export { PokemonGrid } from './components/PokemonGrid';
+export type { Pokemon } from './interfaces/pokemon';
 export type { PokemonsResponse } from './interfaces/pokemons-response';
 export type { SimplePokemon } from './interfaces/simple-pokemon';
+
